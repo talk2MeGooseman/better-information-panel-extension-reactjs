05d2b2d9b96224ff5aa40bbb3525da94f06ec614
Erik Guzman <me.erik@me.com>
2018-05-22 13:22:04 -0700

    WIP for tab form fields

--- src/components/InfoTabForm.js
+++ src/components/InfoTabForm.js
@@ -5,6 +5,9 @@ import FormControl from '@material-ui/core/FormControl';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import TabSectionInfoInputGroup from './TabSectionInfoInputGroup';
+import { Divider } from '@material-ui/core';
+
+const MAX_TABS = 3;
 
 const styles = theme => ({
   container: {
@@ -21,9 +24,22 @@ const styles = theme => ({
 
 class InfoTabForm extends React.Component {
   state = {
-    tabs: [{
+    tabs: []
+  };
 
-    }]
+  componentWillMount() {
+    this.setState({
+      tabs: [this.newInfoTabData()]
+    })
+  }
+
+  newInfoTabData() {
+    return {
+      title: '',
+      textColor: '',
+      bgColor: '',
+      body: '',
+    };
   };
 
   handleChange = event => {
@@ -33,14 +49,35 @@ class InfoTabForm extends React.Component {
     this.setState({ name: event.target.value });
   };
 
+  handleAddTab = () => {
+    if(this.state.tabs.length < MAX_TABS) {
+      let tabs = [...this.state.tabs, this.newInfoTabData()];
+
+      this.setState({
+        tabs: tabs
+      });
+    }
+  };
+
+  renderTabInputs() {
+    return this.state.tabs.map((tab) => {
+      return (
+        <React.Fragment>
+          <TabSectionInfoInputGroup />
+          <Divider/>
+        </React.Fragment>
+      );
+    });
+  }
+
   render() {
     const { classes } = this.props;
 
     return (
       <div className={classes.container}>
         <FormControl className={classes.formControl}>
-          <TabSectionInfoInputGroup />
-          <Button variant="fab" color="primary" aria-label="add" className={classes.button}>
+          { this.renderTabInputs() }
+          <Button onClick={this.handleAddTab} variant="fab" color="primary" aria-label="add" className={classes.button}>
             <AddIcon />
           </Button>
         </FormControl>
