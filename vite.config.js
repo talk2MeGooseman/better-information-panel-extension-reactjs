d84ae4f369578c1016e85d636a7cabf620da675e
Erik Guzman <erik.guzman@codingzeal.com>
2023-01-31 20:26:13 -0800

    Update build configs for pathing

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,9 @@ import svgrPlugin from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   server: {
     port: '8080'
-  },
-  resolve:{
-    alias:{
-    },
-  },
-  build: {
-    rollupOptions: {
-    },
   },
   plugins: [
     react(),
